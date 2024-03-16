24985468680d87ba302878a4f062beb7340c30e7
QI <965297449@qq.com>
2024-03-16 21:29:07 +0800

    cross

---
+++ src/main/java/com/example/demo/config/MyGlobalCorsConfig.java
@@ -0,0 +1,6 @@
+package com.example.demo.config;/**
+* @author  QiYongChuan
+* @Version 1.0
+2024/3/16
+*/public class MyGlobalCorsConfig {
+}
---
+++ src/main/java/com/example/demo/service/WordService.java
@@ -0,0 +1,6 @@
+package com.example.demo.service;/**
+* @author  QiYongChuan
+* @Version 1.0
+2024/3/16
+*/public class WordService {
+}
